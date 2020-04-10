19dcaa645abb2f5bfe9481cd43534085f06aecb7
VladChichin <63444362+VladChichin@users.noreply.github.com>
2020-04-10 15:37:06 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const cartButton = document.querySelector("#cart-button");
+const model = document.querySelector(".model")
+const close = document.querySelector(".close")
+cartButton.addEventListener("click", function(event) {
+    model.classList.add("is-open");
+});
+close.addEventListener("click", function(event) {
+    model.classList.remove("is-open");
+});
+new WOW().init();
